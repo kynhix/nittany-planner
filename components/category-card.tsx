56cb6dde09541334fb8b84975e2136b9f540c271
kynhix <kristopher.t.hicks@gmail.com>
2024-12-09 00:04:55 -0500

    Removed unusable code

--- components/category-card.tsx
+++ components/category-card.tsx
@@ -3,9 +3,7 @@ import { Card, CardContent, CardFooter, CardHeader, CardTitle, } from "@/compone
 import { Category, Task } from "@/lib/core"
 import AddTaskButton from "@/components/add-task-button"
 import { ActiveListContext } from "@/context/active-list-context"
-import { useContext, useState } from "react"
-import { Popover, PopoverTrigger } from "@radix-ui/react-popover"
-import { ModifyTaskPopover } from "./modify-task-popover"
+import { useContext } from "react"
 import { DropdownEditDelete } from "./dropdown-edit-delete"
 import { DotsHorizontalIcon } from "@radix-ui/react-icons"
 
@@ -13,7 +11,6 @@ type CardProps = React.ComponentProps<typeof Card> & { category: Category }
 
 export function CategoryCard({ className, category, ...props }: CardProps) {
   const activeList = useContext(ActiveListContext);
-  const [selectedTask, setSelectedTask] = useState<Task | null>(null);
 
   const deleteCategory = () => {
     activeList.categories = activeList.categories.filter((cat) => cat !== category)
@@ -21,47 +18,12 @@ export function CategoryCard({ className, category, ...props }: CardProps) {
   }
 
   const deleteTask = (task: Task) => {
-    // indexToDelete is -1 initially in case index isn't valid.
-    let indexToDelete = -1;
-    for (let i = 0; i < category.tasks.length; i++) {
-      if (category.tasks[i].id == task.id) {
-        indexToDelete = i;
-        break;
-      }
-    }
-    // Only if the index of the task is found.
-    if (indexToDelete != -1) {
-      category.tasks.splice(indexToDelete, 1);
-    }
+    category.tasks = category.tasks.filter(t => t.id !== task.id)
     activeList.updateContext(activeList)
   };
 
-  // Modify Task Logic
-  const modifyTask = (task: Task | null, action: "open" | "save" | "close", updatedTask?: Task) => {
-    // Opens the popover and selects the task.
-    if (action === "open" && task) {
-      setSelectedTask(task);
-    }
-    // Closes the popover and clears the selected task.
-    if (action === "close") {
-      setSelectedTask(null);
-    }
-    // Saves the task.
-    // Update the task in the category.tasks array.
-    // Closes the popover and clears the selected task.
-    if (action === "save" && updatedTask) {
-      category.tasks = category.tasks.map((t) =>
-        t.id === updatedTask.id ? updatedTask : t
-      );
-      setSelectedTask(null);
-    }
-    activeList.updateContext(activeList);
-  };
-
-
   return (
     <Card className={cn("w-80", "h-fit", className)} {...props}>
-
       <CardHeader>
         <CardTitle className="font-normal text-xl flex justify-between">
           <span>{category.name}</span>
@@ -85,24 +47,6 @@ export function CategoryCard({ className, category, ...props }: CardProps) {
                   <DotsHorizontalIcon />
                 </button>
               </DropdownEditDelete>
-              {/* The modify task popover. */}
-              {selectedTask?.id === task.id && (
-                <Popover
-                  // Opens the popover when its clicked.
-                  open={true}
-                  onOpenChange={(isOpen) => !isOpen && setSelectedTask(null)}>
-                  <PopoverTrigger asChild>
-                    {/*Not sure why, but this div is required for it to render. */}
-                    <div />
-                  </PopoverTrigger>
-                  <ModifyTaskPopover
-                    // Selects a tesk, saves and modifies it, and closes the popover.
-                    task={selectedTask}
-                    onSave={(updatedTask) => modifyTask(null, "save", updatedTask)}
-                    onClose={() => modifyTask(null, "close")}
-                  />
-                </Popover>
-              )}
             </li>
           ))}
         </ul>
